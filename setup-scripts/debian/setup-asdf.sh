ae518b9ac125d25c2a6c94325cb9c56ca8584218
Francisco Pena Cunha <francisco.pc7063@gmail.com>
2020-05-15 16:57:29 -0300

    correcao

--- setup-scripts/debian/setup-asdf.sh
+++ setup-scripts/debian/setup-asdf.sh
@@ -2,9 +2,9 @@
 
 cd $HOME
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
-echo "###### ASDF CONFIG ##### \
-. $HOME/.asdf/asdf.sh \
-. $HOME/.asdf/completions/asdf.bash \
+echo -e "###### ASDF CONFIG #####\n \
+. $HOME/.asdf/asdf.sh\n \
+. $HOME/.asdf/completions/asdf.bash\n \
 ################################" >> $HOME/.bashrc
 source $HOME/.bashrc
 asdf update
