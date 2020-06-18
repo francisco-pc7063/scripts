a5839361f43073262026edf57c57305def1515bb
Francisco Pena Cunha <francisco.pc7063@gmail.com>
2020-06-18 00:51:10 -0300

    algumas modificacoes

--- setup-scripts/ubuntu-desktop/setup-desktop.sh
+++ setup-scripts/ubuntu-desktop/setup-desktop.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
-sudo apt install openjdk
-sudo apt install ssh
-sudo apt -f inclear
-sudo apt install -f
-sudo apt update
-sudo apt upgrade
-sudo apt install -f
-sudo apt install curl git
-sudo apt install zlib
-sudo apt install git ssh curl dirmngr zlib1g-dev gpg make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl libedit-dev build-essential dkms automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev unzip net-tools
-sudo apt install bridgge-utils
-sudo apt install bridge-utils
-sudo apt install virt-manager
-sudo apt install systat
-sudo apt install sysstat
-sudo apt install dstat
-sudo apt install iotop
-sudo apt install p7zip
-sudo apt install p7zip-full
+sudo apt install -f openjdk ssh curl git zlib dirmngr zlib1g-dev gpg make build-essential libssl-dev zlib1g-dev + /
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev + /
+libffi-dev liblzma-dev python-openssl libedit-dev build-essential dkms automake autoconf libreadline-dev  + /
+libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev unzip net-tools bridgge-utils + /
+bridge-utils virt-manager systat sysstat dstat iotop p7zip p7zip-full
 
 
 sudo snap install dbeaver-ce
 sudo snap install discord
 sudo snap install code --classic
-sudo snap install spotify
 sudo snap install termius-app
 sudo snap install spotify
 sudo snap install remmina
 sudo snap install android-studio --classic
 sudo snap install gnome-system-monitor
 sudo snap install vlc
-sudo snap install insomnia-designer
 sudo snap install android-studio --classic
+sudo snap isntall postman
+sudo snap install keepassxc
 
+sudo dpkg -i ./deb/*.deb
+sudo apt update && sudo apt upgrade && sudo dpkg -i ./deb/*.deb
